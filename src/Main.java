2bfdb9b644b7515616743f5e3e3b8f437426fb4d
tiemenbr <tiemenbr@hotmail.nl>
2024-02-02 14:09:47 +0100

    Added person class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Person person = new Person("Tiemen","Br", 19);
+        System.out.println(person);
     }
 }
